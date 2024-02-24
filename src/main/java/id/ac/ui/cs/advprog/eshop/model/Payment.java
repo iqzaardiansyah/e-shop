9264795b101132399015e74068735977a95f376c
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-02-24 20:41:26 +0700

    [GREEN] Implement Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,14 +8,31 @@ import lombok.*;
 public class Payment {
     private String id;
     private String method;
-    private Map<String, String> paymentData;
-    @Setter
     private String status;
-
+    private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
+
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.paymentData = paymentData;
+        }
     }
 
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
